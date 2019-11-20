3c8f036082fde4bcdcfd62e68fea462a2f6316ea
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-20 01:35:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,7 +14,7 @@ end
 
 introduction_with_language("Catherine", "Ruby")
 
-def introduction_with_language_optional(name,language = )
+def introduction_with_language_optional(name,language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
